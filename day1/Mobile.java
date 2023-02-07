b93c9eec19af9d9c254091dddd21666d1b3bb509
Sivaranjani1195 <124480131+Sivaranjani1195@users.noreply.github.com>
2023-02-07 23:22:57 +0530

    Add files via upload
    Day1 Assignments

---
+++ day1/Mobile.java
@@ -0,0 +1,23 @@
+package day1;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String mobileBrandName = "Samsung";
+		System.out.println(mobileBrandName);
+		char mobileLogo = 'S';
+		System.out.println(mobileLogo);
+		short noOfMobilePiece = 3;
+		System.out.println(noOfMobilePiece);
+		int mobileImeiNumber= 12345678;
+		System.out.println(mobileImeiNumber);
+		float mobilePrice = 360000.00f;
+		System.out.println(mobilePrice);
+		boolean isDamaged = false;
+		System.out.println(isDamaged);
+		
+
+	}
+
+}
---
+++ day1/TwoWheeler.java
@@ -0,0 +1,24 @@
+package day1;
+
+public class TwoWheeler {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int noOfWheels = 2;
+		System.out.println(noOfWheels);
+		short noOfMirrors = 2;
+		System.out.println(noOfMirrors);
+		long chassisNumber =34345;
+		System.out.println(chassisNumber);
+		boolean isPunctured = true;
+		System.out.println(isPunctured);
+		String bikeName = "Apache";
+		System.out.println(bikeName);
+		double runningKM = 7000000;
+		System.out.println(runningKM);
+		
+		
+
+	}
+
+}
